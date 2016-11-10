828b2a28e6822ff3e0815a82500d8f6a9c4fdd48
hcarlson1 <hcarlson1@css.edu>
2016-11-10 11:02:49 -0600

    What we did in class.

---
+++ CurrencyTrader/AsynchUrlTradeDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader
+{
+    public class AsynchUrlTradeDataProvider : ITradeDataProvider
+    {
+        private readonly String url;
+        UrlTradeDataProvider SynchTradeProvider;
+        public AsynchUrlTradeDataProvider(String url)
+        {
+            SynchTradeProvider = new UrlTradeDataProvider(url);
+            this.url = url;
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            Task.Run(() => SynchTradeProvider.GetTradeData());//this creates a blank named instance and now we just run the gettradedata once
+            //return SynchTradeProvider.GetTradeData();
+        }
+    }
+}
